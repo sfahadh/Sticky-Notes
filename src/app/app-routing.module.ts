3c2044694978b6c90e6e9d04e9e55729fa75bb9f
sfahadh <sfahadh14@gmail.com>
2020-12-06 11:21:51 -0500

    added routes to todo list component and form component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { FormComponent } from './components/form/form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TodoListComponent },
+  { path: 'todo', component: FormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+  
 export class AppRoutingModule { }
