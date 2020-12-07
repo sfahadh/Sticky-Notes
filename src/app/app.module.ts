b3d9acff35365c37ad640eacf9b1558f3cd598a0
sfahadh <sfahadh14@gmail.com>
2020-12-07 11:50:25 -0500

    moved footer btn and todo list component under home folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FormComponent } from './components/form/form/form.component';
-import { TodoListComponent } from './components/todo-list/todo-list.component';
-import { FooterBtnComponent } from './components/footer-btn/footer-btn.component';
+import { TodoListComponent } from './components/home/todo-list/todo-list.component';
+import { FooterBtnComponent } from './components/home/footer-btn/footer-btn.component';
 import { DropdownComponent } from './components/form/dropdown/dropdown.component';
 import { InputsComponent } from './components/form/inputs/inputs.component';
 import { HomeComponent } from './components/home/home/home.component';
