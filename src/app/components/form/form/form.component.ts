0ec0478818972e7990f6bfa3a10578d2a090b60b
sfahadh <sfahadh14@gmail.com>
2020-12-07 11:43:33 -0500

    imported router in form component to navigate back to home after clicking form button

--- src/app/components/form/form/form.component.ts
+++ src/app/components/form/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-form',
@@ -7,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
   
 export class FormComponent implements OnInit {
-  constructor() { }
+  constructor(private Router: Router) { }
   ngOnInit(): void { }
+
+  save() {
+    this.Router.navigate(['']);
+  }
 }
