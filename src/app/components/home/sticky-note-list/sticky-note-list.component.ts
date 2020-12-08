f899ed670459f6d351bd1f5db38bd2d89b6ebeac
sfahadh <sfahadh14@gmail.com>
2020-12-08 10:50:45 -0500

    moved code from todo list to sticky note list

--- src/app/components/home/sticky-note-list/sticky-note-list.component.ts
+++ src/app/components/home/sticky-note-list/sticky-note-list.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { StickyNote } from 'src/app/model/sticky-note';
+import { StickyNotesService } from 'src/app/services/sticky-note.service';
 
 @Component({
   selector: 'app-sticky-note-list',
   templateUrl: './sticky-note-list.component.html',
   styleUrls: ['./sticky-note-list.component.css']
 })
+  
 export class StickyNoteListComponent implements OnInit {
+  stickyNotes: StickyNote[];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private stickyNoteService: StickyNotesService) { }
+  ngOnInit(): void { 
+    this.stickyNotes = this.stickyNoteService.getAllStickyNotes();
+    console.log(this.stickyNotes);
   }
-
 }
