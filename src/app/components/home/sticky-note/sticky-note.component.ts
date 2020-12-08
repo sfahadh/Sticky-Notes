4dc7e1d09d64400bd2eab7a8f0b0328a897e05a0
sfahadh <sfahadh14@gmail.com>
2020-12-08 10:56:12 -0500

    passed sticky note data from parent to child and moved some code from sticky note list to sticky note component

--- src/app/components/home/sticky-note/sticky-note.component.ts
+++ src/app/components/home/sticky-note/sticky-note.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { StickyNote } from 'src/app/model/sticky-note';
 
 @Component({
   selector: 'app-sticky-note',
   templateUrl: './sticky-note.component.html',
   styleUrls: ['./sticky-note.component.css']
 })
+  
 export class StickyNoteComponent implements OnInit {
-
+  @Input() stickyNote: StickyNote;
+  
   constructor() { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
 }
