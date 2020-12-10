9f7f55c67cd5715f67a817b0fb415a4edb636c4b
sfahadh <sfahadh14@gmail.com>
2020-12-10 10:55:21 -0500

    passed data from home component to filter component

--- src/app/components/home/filter/filter.component.ts
+++ src/app/components/home/filter/filter.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { StickyNote } from 'src/app/model/sticky-note';
 
 @Component({
   selector: 'app-filter',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
   
 export class FilterComponent implements OnInit {
+  @Input() stickyNotes: StickyNote[];
   colorSelected: string;
   colors: string[] = ["blue", "gray", "green", "red", "yellow", "turquoise", "black", "white"];
 
