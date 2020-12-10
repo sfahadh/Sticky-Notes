18dcb4668b49abf587ff451701d6de5179db5fb6
sfahadh <sfahadh14@gmail.com>
2020-12-10 11:13:07 -0500

    passed colorSelected data from filter component to sticky note list component

--- src/app/components/home/sticky-note-list/sticky-note-list.component.ts
+++ src/app/components/home/sticky-note-list/sticky-note-list.component.ts
@@ -10,6 +10,7 @@ import { StickyNotesService } from 'src/app/services/sticky-note.service';
   
 export class StickyNoteListComponent implements OnInit {
   @Input() stickyNotes: StickyNote[];
+  @Input() colorSelected: string;
 
   constructor() { }
   ngOnInit(): void { }
