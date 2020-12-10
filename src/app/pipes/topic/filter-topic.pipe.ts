27c77280aea4dd061ef2e1002f362065275d08ba
sfahadh <sfahadh14@gmail.com>
2020-12-10 13:05:30 -0500

    adjusted filter topic pipe to filter sticky notes by topic

--- src/app/pipes/topic/filter-topic.pipe.ts
+++ src/app/pipes/topic/filter-topic.pipe.ts
@@ -1,12 +1,13 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { StickyNote } from 'src/app/model/sticky-note';
 
 @Pipe({
   name: 'filterTopic'
 })
+  
 export class FilterTopicPipe implements PipeTransform {
-
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(stickyNotes: StickyNote[], topic: string): StickyNote[] {
+    if (topic === undefined || topic === "") return stickyNotes;
+    return stickyNotes.filter(stickyNote => stickyNote.topic.startsWith(topic));
   }
-
 }
